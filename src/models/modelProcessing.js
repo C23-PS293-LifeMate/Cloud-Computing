dad9b3c5c8dadca93e99a1ee643a30ccc534570a
Joshevan <joshevan@ui.ac.id>
2023-06-05 00:48:20 +0700

    stress model and return record date

--- src/models/modelProcessing.js
+++ src/models/modelProcessing.js
@@ -10,11 +10,15 @@ async function loadModel(body) {
   if (body.gender === 'perempuan'){
     body.gender = '0';
   }
+  const model = await tf.loadLayersModel('file://src/models/stressModel.json');
   const valuesArray = Object.entries(body).map(([key, value]) => value);
   const bmiIndexs = [7, 1, 2];
+  const stressIndexs = [3, 4, 5, 6];
   const bmiInput = bmiIndexs.map(index => valuesArray[index]);
+  const stressInputRaw = stressIndexs.map(index => valuesArray[index]);
+  const stressInput = tf.tensor2d(stressInputRaw, [1,4], 'float32')
   const bmiOutput = bmiModels.bmiPredict(bmiInput)
-  const stressOutput = 5 
+  const stressOutput = await model.predict(stressInput).arraySync()[0][0]
   const predictOutput = {
     bmiOutput: bmiOutput,
     stressOutput: stressOutput
